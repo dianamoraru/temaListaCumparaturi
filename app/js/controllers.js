4e5f117fadcb4dfe063462462cdf32371fcf629b
dianamoraru <moraru.diana90@yahoo.com>
2013-11-25 01:50:07 +0200

    adauga produs in prima pagina din a doua

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,20 @@
 /* Controllers */
 
 var shoppingControllers = angular.module('shoppingControllers', []);
+var uid=0;
+
+shoppingControllers.factory('productFactory',function(){
+	var products=[];
+	var factory={};
+
+	
+
+	factory.getProducts=function(){
+		return products;
+	}
+	return factory;
+
+});
 
 shoppingControllers.controller('ListController', function($scope) {
    $scope.lists = ['L1','L2'];
@@ -11,10 +25,32 @@ shoppingControllers.controller('ListController', function($scope) {
    }
 
 });
-shoppingControllers.controller('ProductController', function($scope) {
-   $scope.products = ['P1','P2'];
-   $scope.addProduct=function(){
-	 $scope.products.push($scope.newProduct);
-   }
+shoppingControllers.controller('ProductController', function($scope,productFactory) {
+
+	$scope.addProduct=function(){
+    	if($scope.newProduct.id==null){
+    		$scope.newProduct.id=uid++;
+    		$scope.products.push($scope.newProduct);
+    	}else{
+    		for(i in $scope.products){
+    			if($scope.products[i].id==$scope.newProduct.id){
+    				$scope.products[i]=$scope.newProduct;
+    			}
+    		}
+    	}
+     $scope.newProduct={};
+	}
+	$scope.editProduct=function(id){
+		$location.path("href='#/adauga'");
+		for(i in $scope.products){
+			if ($scope.products[i].id===id) {
+				$scope.newProduct=angular.copy($scope.products[i]);
+			};
+		}
+	}
+
+$scope.products = productFactory.getProducts();
+    
+
 });
 // {'name':'P1','description':'D1'},{'name':'P2','description':'D2'}
