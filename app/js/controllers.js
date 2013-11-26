675fd3179bdaf60e42138787cdccd8f88cff5475
dianamoraru <moraru.diana90@yahoo.com>
2013-11-26 21:07:02 +0200

    modificare+stergere functionala, liste nefunc

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,76 +1,70 @@
-'use strict';
-
-/* Controllers */
-
 var shoppingControllers = angular.module('shoppingControllers', []);
-var uid=0;
+var uid = 0;
 
+shoppingControllers.controller('ListController', function($scope) {
+   $scope.lists = [];
+   $scope.addList=function(){
+	 $scope.lists.push($scope.newList);
+   $scope.lists = productFactory.getLists();
+
+   }
+
+});
 shoppingControllers.factory('productFactory',function(){
-	var products=[];
-	var newProduct=[];
-	var factory={};
+  var products=[];
+  var newProduct=[];
+  var factory={};
+   factory.getProducts=function(){
+    return products;
+  }
+  return factory;
 
-	
-
-	factory.getProducts=function(){
-		return products;
-	}
-	// factory.setProdcut=function(i){
-	// 	return newProduct=products[i];
-	// }
-	return factory;
+});
+shoppingControllers.factory('listFactory',function(){
+  var lists=[];
+  var factory2={};
+   factory.getLists=function(){
+    return lists;
+  }
+  return factory2;
 
 });
 
-// shoppingControllers.controller('ListController', function($scope) {
-//    $scope.lists = ['L1','L2'];
-//    $scope.addList=function(){
-// 	 $scope.lists.push($scope.newList);
-//    }
 
-// });
-
-shoppingControllers.controller('ProductController', function($scope,$location,productFactory) {
-	$scope.addProduct=function(){
-    	if($scope.newProduct.id==null){
-    		$scope.newProduct.checked=false;
-    		$scope.newProduct.id=uid++;
-    		$scope.products.push($scope.newProduct);
-    	}else{
-    		for(i in $scope.products){
-    			if($scope.products[i].id==$scope.newProduct.id){
-    				$scope.products[i]=$scope.newProduct;
-    			};
-    		};
-    	};
+shoppingControllers.controller('ProductController', function($scope,productFactory) {
+  $scope.addProduct=function(){
+      if($scope.newProduct.id==null){
+        $scope.newProduct.checked=false;
+        $scope.newProduct.id=uid++;
+        $scope.products.push($scope.newProduct);
+      }else{
+        for(i in $scope.products){
+          if($scope.products[i].id==$scope.newProduct.id){
+            $scope.products[i]=$scope.newProduct;
+          };
+        };
+      };
      $scope.newProduct={};
-	};
+  };
 
-	$scope.editProduct=function(id){
-		$location.path('/adauga');
-		for(i in $scope.products){
+  $scope.editProduct=function(id){
+    for(i in $scope.products){
+      if ($scope.products[i].id===id) {
 
-			if ($scope.products[i].id===id) {
+      $scope.newProduct=angular.copy($scope.products[i]);
+      };
+    };
+  };
+  $scope.removeProduct=function(){
 
-				$scope.newProduct=angular.copy($scope.products[i]);
-				// $scope.newProduct=copy(factory.newProduct(i));
-			};
-		};
-	};
+    i=$scope.products.length;
+    while(i--){
+      if ($scope.products[i].checked===true) {
+        $scope.products.splice(i, 1);
+      }
+    }
+  };
+$scope.products = productFactory.getProducts();
 
-	$scope.removeProduct=function(){
-	
-	 	angular.forEach($scope.products, function (product, index) {
-	 		
-	 		if(product.checked===true){
-	 			var index = $scope.products.indexOf(product);
-				$scope.products.splice(index, 1);
-
-	 		};
-	 	});
-     };
-
-	$scope.products = productFactory.getProducts();
+  
 });
-
-// {'name':'P1','description':'D1'},{'name':'P2','description':'D2'}
