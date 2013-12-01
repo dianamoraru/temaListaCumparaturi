4683e3697c2a829273229cfed3e70003dd69be3d
dianamoraru <moraru.diana90@yahoo.com>
2013-12-01 12:07:58 +0200

    produs selectat taiat
    +scoatere clase,id-uri nefolosite
    +spatiere

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,20 +10,17 @@ shoppingControllers.factory('productFactory',function(){
    {'id':3,'list':'Lista2','checked':false,'name':'Margarina','description':'','category':'Alimentare'},
   {'id':4,'list':'Lista2','checked':false,'name':'Mancare pisica','description':'3 Kg','category':'Hrana Animale'},
   {'id':5,'list':'Lista2','checked':false,'name':'Uscator par','description':'','category':'Electronice'}
-
-
   ];
   var factory={};
-   factory.getProducts=function(){
-    return products;
+    factory.getProducts=function(){
+      return products;
   }
   return factory;
 });
 
 shoppingControllers.factory('categoriesFactory',function(){
   
-   var categories=["Alimentare", "Nealimentare",
-   "Hrana Animale","Produse Copii","Electronice"];
+  var categories=["Alimentare", "Nealimentare","Hrana Animale","Produse Copii","Electronice"];
   var factory={};
   factory.getCategories=function(){
     return categories;
@@ -31,7 +28,7 @@ shoppingControllers.factory('categoriesFactory',function(){
   return factory;
 });
 
-shoppingControllers.factory('listFactory',function(){
+shoppingControllers.factory('listsFactory',function(){
   var lists=['Lista1','Lista2'];
   var factory={};
    factory.getLists=function(){
@@ -41,8 +38,7 @@ shoppingControllers.factory('listFactory',function(){
 
 });
 
-
-shoppingControllers.controller('ProductController', function($window,$scope,productFactory,categoriesFactory,listFactory) {
+shoppingControllers.controller('ProductController', function($window,$scope,productFactory,categoriesFactory,listsFactory) {
   $scope.addProduct=function(){
     if($scope.newProduct.name==""||$scope.newProduct.name==null){
        $window.alert('Numele produsului este obligatoriu!');
@@ -58,17 +54,16 @@ shoppingControllers.controller('ProductController', function($window,$scope,prod
           };
         };
       };
-     var lista=$scope.newProduct.list;
-     $scope.newProduct={};
-     $scope.newProduct.category='Alimentare';
-     $scope.newProduct.list=lista;
-    }
+      var lista=$scope.newProduct.list;
+      $scope.newProduct={};
+      $scope.newProduct.category='Alimentare';
+      $scope.newProduct.list=lista;
+    };
   };
 
   $scope.addList=function(){
     $scope.lists.push($scope.newList);
     $scope.newList='';
-    
   };
 
 
@@ -76,22 +71,22 @@ shoppingControllers.controller('ProductController', function($window,$scope,prod
     $scope.displayAddProduct=true;
     for(i in $scope.products){
       if ($scope.products[i].id==id) {
-
-      $scope.newProduct=angular.copy($scope.products[i]);
+        $scope.newProduct=angular.copy($scope.products[i]);
       };
     };
   };
-  $scope.removeProduct=function(){
 
+  $scope.removeProduct=function(){
     i=$scope.products.length;
     while(i--){
       if ($scope.products[i].checked==true) {
         $scope.products.splice(i, 1);
-      }
-    }
+      };
+    };
   };
-$scope.products = productFactory.getProducts();
-$scope.categories=categoriesFactory.getCategories();
-$scope.lists = listFactory.getLists();
+  
+  $scope.products = productFactory.getProducts();
+  $scope.categories=categoriesFactory.getCategories();
+  $scope.lists = listsFactory.getLists();
   
 });
